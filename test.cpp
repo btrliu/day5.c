59e9718b1eaecd01f448335f8da76a560ba6f8ae
btrliu <141817822+btrliu@users.noreply.github.com>
2023-08-11 23:10:02 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	int sum = 0;
+//	printf("你为什么会没豆？");
+//	scanf("%d", &sum);
+//	if (sum = 1)
+//	{
+//		printf("少壮不努力");
+//	}
+//	else
+//	{
+//		printf("老大徒伤悲");
+//	}
+//		return 0;
+//}
+
+int main()
+{
+	int c = 0;
+	while(c < 30)
+	{
+		printf("敲代码：%d\n",c);
+		c++;
+	}
+	if (c == 30)
+	{
+		printf("人生巅峰");
+	}
+}
